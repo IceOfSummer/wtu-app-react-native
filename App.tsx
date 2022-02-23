5c0fcbd6719969a92b36eeff76709c827d9e2061
HuPeng333 <2237803016@qq.com>
2022-02-23 13:52:32 +0800

    update: 优化tabBar目录结构

--- App.tsx
+++ App.tsx
@@ -1,45 +1,8 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NavigationContainer } from '@react-navigation/native'
-import Home from './src/tabs/Home'
-import Applications from './src/tabs/Applications'
-import ClassSchedule from './src/tabs/ClassSchedule'
-import PersonalCenter from './src/tabs/PersonalCenter'
-import Icons from './src/utils/Icons'
-
-const Tab = createBottomTabNavigator()
-const HOME = 'Home'
-const CLASS_SCHEDULE = 'ClassSchedule'
-const APPLICATION = 'Applications'
-const PERSONAL_CENTER = 'PersonalCenter'
+import TabBar from './src/tabs'
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: param => {
-            switch (route.name) {
-              case HOME:
-                return <Icons iconText="&#xe613;" {...param} />
-              case CLASS_SCHEDULE:
-                return <Icons iconText="&#xe62c;" {...param} />
-              case APPLICATION:
-                return <Icons iconText="&#xe603;" {...param} />
-              case PERSONAL_CENTER:
-                return <Icons iconText="&#xe608;" {...param} />
-              default:
-                throw new Error('unknown route name')
-            }
-          },
-        })}>
-        <Tab.Screen name={HOME} component={Home} />
-        <Tab.Screen name={CLASS_SCHEDULE} component={ClassSchedule} />
-        <Tab.Screen name={APPLICATION} component={Applications} />
-        <Tab.Screen name={PERSONAL_CENTER} component={PersonalCenter} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  )
+  return <TabBar />
 }
 
 export default App
---
+++ src/tabs/index.tsx
@@ -0,0 +1,48 @@
+import { NavigationContainer } from '@react-navigation/native'
+import Icons from '../utils/Icons'
+import Home from './Home'
+import ClassSchedule from './ClassSchedule'
+import Applications from './Applications'
+import PersonalCenter from './PersonalCenter'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator()
+const HOME = 'Home'
+const CLASS_SCHEDULE = 'ClassSchedule'
+const APPLICATION = 'Applications'
+const PERSONAL_CENTER = 'PersonalCenter'
+
+const TabBar = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: param => {
+            switch (route.name) {
+              case HOME:
+                return <Icons iconText="&#xe613;" {...param} />
+              case CLASS_SCHEDULE:
+                return <Icons iconText="&#xe62c;" {...param} />
+              case APPLICATION:
+                return <Icons iconText="&#xe603;" {...param} />
+              case PERSONAL_CENTER:
+                return <Icons iconText="&#xe608;" {...param} />
+              default:
+                throw new Error('unknown route name')
+            }
+          },
+          header: () => {
+            return null
+          },
+        })}>
+        <Tab.Screen name={HOME} component={Home} />
+        <Tab.Screen name={CLASS_SCHEDULE} component={ClassSchedule} />
+        <Tab.Screen name={APPLICATION} component={Applications} />
+        <Tab.Screen name={PERSONAL_CENTER} component={PersonalCenter} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default TabBar
