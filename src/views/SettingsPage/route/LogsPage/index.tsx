db78afadbd748912e3c87e8d3141d474504b58dd
DeSen Xu <2237803016@qq.com>
2023-02-11 12:41:31 +0800

    update: 支持日志本地预览

--- src/views/SettingsPage/route/LogsPage/index.tsx
+++ src/views/SettingsPage/route/LogsPage/index.tsx
@@ -1,13 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { getLogFiles, getLogPath } from '../../../../utils/LoggerUtils'
+import React, { useEffect, useRef, useState } from 'react'
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native'
+import {
+  getLogFiles,
+  getLogger,
+  getLogPath,
+} from '../../../../utils/LoggerUtils'
 import Button from 'react-native-button'
 import { SpringScrollView } from 'react-native-spring-scrollview'
 import Share from 'react-native-share'
 import Toast from 'react-native-root-toast'
+import RNFS from 'react-native-fs'
+import Drawer from '../../../../component/Drawer'
+import DrawerCommonContainer from '../../../../component/Drawer/DrawerCommonContainer'
+
+const logger = getLogger('/views/SettingsPage/route/LogsPage')
 
 const LogsPage: React.FC = () => {
   const [logs, setLogs] = useState<Array<string>>([])
+  const [logText, setLogTexts] = useState('')
+  const logPreviewDrawer = useRef<Drawer>(null)
 
   const checkLog = (_logName: string) => {
     Share.open({
@@ -20,8 +31,21 @@ const LogsPage: React.FC = () => {
       if (msg === 'User did not share') {
         return
       }
+      logger.error('share failed: ' + e.message)
       Toast.show('分享失败: ' + e.message)
     })
+  }
+
+  const openLogPreview = (_logName: string) => {
+    RNFS.readFile('file://' + getLogPath(_logName), 'utf8')
+      .then(r => {
+        setLogTexts(r)
+        logPreviewDrawer.current?.showDrawer()
+      })
+      .catch(e => {
+        logger.error('read file failed: ' + e.message)
+        Toast.show('预览日志失败: ' + e.message)
+      })
   }
 
   useEffect(() => {
@@ -34,6 +58,7 @@ const LogsPage: React.FC = () => {
     <SpringScrollView>
       {logs.map(value => (
         <Button
+          onLongPress={() => openLogPreview(value)}
           key={value}
           containerStyle={styles.buttonContainer}
           onPress={() => checkLog(value)}>
@@ -43,6 +68,13 @@ const LogsPage: React.FC = () => {
         </Button>
       ))}
       <Text style={global.styles.infoTipText}>30天前的日志会自动删除</Text>
+      <Drawer ref={logPreviewDrawer}>
+        <ScrollView style={{ height: Dimensions.get('window').height / 2 }}>
+          <DrawerCommonContainer title="日志预览" buttonText="">
+            <Text>{logText}</Text>
+          </DrawerCommonContainer>
+        </ScrollView>
+      </Drawer>
     </SpringScrollView>
   )
 }
